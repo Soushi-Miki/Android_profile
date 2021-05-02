14824df79a66259a7578e504434ec8399a424357
Soushi-Miki <s2081145@s.konan-u.ac.jp>
2021-05-02 17:25:53 +0900

    プロフィールアプリ応用の完成

--- app/src/main/java/app/miki/mikiso/profile/MainActivity.kt
+++ app/src/main/java/app/miki/mikiso/profile/MainActivity.kt
@@ -2,7 +2,6 @@ package app.miki.mikiso.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,19 +9,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //画像を見えない表示にする
-        profileImage.isVisible = false
 
-        //テキストを見えない状態にする
-        profileCommentText.isVisible = false
-
+        //ボタンがタップされた時に
         button1.setOnClickListener{
 
-            //画像を見える状態にする
-            profileImage.isVisible = true
-
-            //テキストを見える状態にする
-            profileCommentText.isVisible = true
+            //画像をImageViewに反映する
+            profileImage.setImageResource(R.drawable.randy_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text="名前"
+            //コメントをTextViewに反映する
+            profileCommentText.text="Androidメンターのランディだよ"
+        }
+        //スポーツのボタンがタップされた時に
+        button2.setOnClickListener{
+            //画像をImageViewに反映する
+            profileImage.setImageResource(R.drawable.baseball_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text="スポーツ"
+            //コメントをTextViewに反映する
+            profileCommentText.text="野球観戦が好きでスタジアムに通っている"
+        }
+        //好きな食べ物のボタンがタップされた時に
+        button3.setOnClickListener{
+            //画像をImageViewに反映する
+            profileImage.setImageResource(R.drawable.donut_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text="好きな食べ物"
+            //コメントをTextViewに反映する
+            profileCommentText.text="キャンディやドーナツが大好き"
+        }
+        //趣味のボタンがタップされた時に
+        button4.setOnClickListener{
+            //画像をImageViewに反映する
+            profileImage.setImageResource(R.drawable.gadget_image)
+            //ラベルをTextViewに反映する
+            profileLabelText.text="趣味"
+            //コメントをTextViewに反映する
+            profileCommentText.text="ガジェットを集めて動かすこと"
         }
 
     }
